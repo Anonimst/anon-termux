63f2ae2f9ec9fa180c656cbc7d30826aee1632f0
Anonimst <60968428+Anonimst@users.noreply.github.com>
2020-02-12 23:11:00 +0300

    Update gaytroll.py

--- gaytroll.py
+++ gaytroll.py
@@ -1,24 +1,23 @@
 import vk, time, os
 os.system('clear')
-
 pubs = [151233321,65797052,102413048,89243825,61515264,88667014,180103853,101105077,89148442,157274855,52771476,68284944,74383909,114285300,88026464,40673151,160175019,164902979,161144333,126904164,85677013]
-def troll ():
+def gaytroll ():
 	w = open ('backup', 'w')
 	w.write (api.status.get()['text'] + '\n')
 	api.status.set(text="Люблю чёрные и волсатые хуйцы! Пишите!")
 	print ("\n\033[32mПишем статус...")
 	time.sleep (0.5)
 	for group in groups:
-		wwrite (str(group) + '\n')
+		w.write (str(group) + '\n')
 		api.groups.leave(group_id=group)
 		time.sleep(0.25)
-	wclose ()
+	w.close ()
 	print ("\033[FВступаем в гей паблики...  ")
 	for pub in pubs:
 	        api.groups.join(group_id=pub)
 	        time.sleep(0.25)
 	print ("\033[FDone:)                       \n")
-def untroll ():
+def ungaytroll ():
 	r = open ('Восстоновление', 'r')
 	api.status.set(text=r.readline())
 	print ("\n\033[32mПокидаем гей паблики...")
@@ -75,6 +74,6 @@ except:
 id = api.users.get()[0]['id']
 groups = api.groups.get(user_id=id)['items']
 if option == '1':
-	troll ()
+	gaytroll ()
 else:
-	untroll ()
+	ungaytroll ()
